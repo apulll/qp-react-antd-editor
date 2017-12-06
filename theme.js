c639c51e675e7ad9c74a4267deb4a9a1a0a690be
capul <1216065136@qq.com>
2017-12-06 18:15:33 +0800

    禁用 theme

--- theme.js
+++ theme.js
@@ -1,12 +1,12 @@
 module.exports = () => {
   return {
-    'primary-color': '#22a666',
-    'blue-6':'#22a666',
-    'link-color': '#22a666',
-    'btn-primary-bg':'#22a666',
-    'outline-color' : '#22a666',
-    'input-hover-border-color':'#22a666',
-    'process-default-color':'#22a666',
-    'border-radius-base': '2px',
+    // 'primary-color': '#22a666',
+    // 'blue-6':'#22a666',
+    // 'link-color': '#22a666',
+    // 'btn-primary-bg':'#22a666',
+    // 'outline-color' : '#22a666',
+    // 'input-hover-border-color':'#22a666',
+    // 'process-default-color':'#22a666',
+    // 'border-radius-base': '2px',
   };
 };
